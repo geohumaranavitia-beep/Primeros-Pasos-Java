f182c6d731c5eb0d610f136161101c2ebbcbce68
geohumaranavitia-beep <geohumaranavitia@gmail.com>
2026-09-17 18:12:24 -0600

    Add files via upload
    # ☕ Mis Primeros Pasos en Java Puro: Rompiendo la Sintaxis (V1.0)
    
    ¡Hola! Este repositorio documenta mi transición de la robótica (C++ con Arduino) hacia el desarrollo de software puro utilizando **Java**. Este proyecto fue desarrollado de forma independiente durante mi tercer semestre de preparatoria en Durango, México.
    
    El objetivo de este código fue dominar la estructura lógica y sintáctica de Java, un lenguaje de tipado estricto que requiere una arquitectura de cajas de seguridad (Clases y Métodos) para poder procesar información.
    
    ---
    
    ## 💻 El Código Desarrollado (Validador de Edad)
    El programa utiliza variables de tipo entero (`int`) y lógica condicional pura (`if / else`) para evaluar datos de entrada directamente en el procesador y transformarlos en información de salida en la consola de texto.
    
    ```java
    public class Main {
        public static void main(String[] args) {
            int edad = 16;
    
            if (edad == 16) {
                System.out.println("prepara las maletas para el hotel riu");
            } else {
                System.out.println("Aun falta para el viaje");
            }
        }
    }
    ```
    
    ---
    
    ## 🧠 Aprendizajes y Desafíos Superados
    * **Arquitectura Orientada a Objetos:** Aprendí que en Java ningún comando puede quedar flotando suelto en el aire; todo debe vivir encapsulado dentro de `public class Main` y del método `public static void main`.
    * **Control de Sintaxis Crítica:** Dominé la importancia de las mayúsculas en los comandos del sistema (`System` y `String`) y el uso de comillas dobles (`"`) en lugar de comillas simples repetidas.
    * **Uso de Herramientas Profesionales:** Utilicé compiladores en línea y el sistema de autocompletado para verificar la sintaxis en tiempo real y optimizar la escritura de código limpio.
    
    ---
    *Siguiente Reto: Implementar la captura de datos dinámica mediante el teclado (`Scanner`) para la creación de minijuegos interactivos de texto.*

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main{
+  public static void main(String[] args){
+      
+  
+int edad = 16;
+if (edad == 16) {
+    System.out.println("prepara las maletas para el hotel riu");
+} else {
+    System.out.println("Aun falta para el viaje");
+    
+       }
+
+    }
+  
+ }
